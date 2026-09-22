a7aba7ddbcacb6aa439ff7da37c2d58fed4b7ffa
Tessica <tgom0006@student.monash.edu>
2026-09-23 00:06:57 +1000

    Binary Trees (BT) scaffold added

--- Linked Lists.cpp
+++ Linked Lists.cpp
@@ -2,66 +2,78 @@
 using namespace std;
 
 template <typename T>
-struct Node {
+struct Node
+{
 
-    T item; 
+    T item;
     Node *next = nullptr;
     Node *prev = nullptr;
-
 };
 
 template <typename T>
-struct LinkedList {
-    Node<T>* head = nullptr;
-    Node<T>* tail = nullptr;
-    
+struct LinkedList
+{
+    Node<T> *head = nullptr;
+    Node<T> *tail = nullptr;
+
     int size = 0;
-    
+
     // TODO: determine what should the input of the method be and implement it
-    void append(T item) {
-        Node<T>* new_node_ptr = new Node<T>;
+    void append(T item)
+    {
+        Node<T> *new_node_ptr = new Node<T>;
         new_node_ptr->item = item;
 
         // If there are no nodes in my chain, this will be the first and last one
-        if (head == nullptr) {
+        if (head == nullptr)
+        {
             head = tail = new_node_ptr;
-            new_node_ptr->prev = nullptr; //nth before so null
+            new_node_ptr->prev = nullptr; // nth before so null
 
             size++;
-            
+
             return;
         }
 
         // Last one is already stored so add the pointer to the last one
         tail->next = new_node_ptr;
-        new_node_ptr->prev = tail; //store prev 
-        tail = new_node_ptr; 
+        new_node_ptr->prev = tail; // store prev
+        tail = new_node_ptr;
 
         size++;
     }
-    
-    Node<T>* get_node_at_index(int index) {
-        if (index < 0){return nullptr;}
 
-        Node<T>* current_node = head;
-        while (index > 0 && current_node != nullptr) {
+    Node<T> *get_node_at_index(int index)
+    {
+        if (index < 0)
+        {
+            return nullptr;
+        }
+
+        Node<T> *current_node = head;
+        while (index > 0 && current_node != nullptr)
+        {
             current_node = current_node->next;
             index--;
         }
         return current_node;
     }
-    
+
     // TODO: determine what should the input of the method be and implement it
-    void set_index(int index, T item) {
-        Node<T>* node_at_index = get_node_at_index(index);
-        if (node_at_index != nullptr) {
+    void set_index(int index, T item)
+    {
+        Node<T> *node_at_index = get_node_at_index(index);
+        if (node_at_index != nullptr)
+        {
             node_at_index->item = item;
         }
     }
-    
-    void display() {
-        Node<T>* temp = head;
-        while (temp != nullptr) {
+
+    void display()
+    {
+        Node<T> *temp = head;
+        while (temp != nullptr)
+        {
             cout << temp->item << " ";
             temp = temp->next;
         }
@@ -71,15 +83,18 @@ struct LinkedList {
 
 int tests_run = 0, tests_failed = 0;
 
-void check(bool condition, const string& name) {
+void check(bool condition, const string &name)
+{
     tests_run++;
-    if (!condition) {
+    if (!condition)
+    {
         tests_failed++;
         cout << "FAIL: " << name << endl;
     }
 }
 
-int main() {
+int main()
+{
     // // --- empty list ---
     // LinkedList<int> empty;
     // check(empty.size == 0, "empty: size is 0");
@@ -157,14 +172,14 @@ int main() {
     list.append(10);
     list.append(20);
     list.append(30);
-    
+
     cout << "Linked List: ";
     list.display();
-    
+
     list.set_index(1, 25);
     cout << "Linked List after setting index 1 to 25: ";
     list.display();
-    
+
     cout << "Item at index 2: " << list.get_node_at_index(2)->item << endl;
     return 0;
 }
